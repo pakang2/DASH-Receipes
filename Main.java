bd166a125e92225db02ea6f6b345b7aa17b1ed42
pakang2 <123673517+pakang2@users.noreply.github.com>
2023-05-29 14:02:43 -0400

    RecipeInfo removed
    recipe info scanner moved to main

--- Main.java
+++ Main.java
@@ -6,6 +6,12 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        while(input.hasNextLine())
+        {
+            
+        }
+        
         System.out.println("Please type a number to select a recipe: ");
         
         int inputByUser = input.nextInt();
